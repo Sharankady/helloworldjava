40f215b81d52b488ec3562c52bd321aefa0b9cbc
Sharankady <106224485+Sharankady@users.noreply.github.com>
2022-07-05 22:46:41 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 5th july webhooks triggered by webhook");
+    System.out.println("Hello World 5th july new webhooks triggered by webhook");
     }
 }
